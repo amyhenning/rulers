a0a479a4f7fb52b44813ea8182d4115a8a9bded2
amyhenning <henning.amy@gmail.com>
2020-06-26 15:55:19 -0500

    Some error handling experiments

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,12 +8,19 @@ module Rulers
       if env["PATH_INFO"] == '/favicon.ico'
         return [404,
           {'Content-Type' => 'text/html'}, []]
+      elsif env["PATH_INFO"] == '/'
+        controller = QuotesController.new(env)
+        act = 'a_quote'
+      else
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
       end
-
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      begin
+        text = controller.send(act)
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      rescue Exception => error
+        [500, {'Content-Type' => 'text/html'}, ["Boohoo, an error occurred: #{error}"]]
+      end
     end
   end
 
